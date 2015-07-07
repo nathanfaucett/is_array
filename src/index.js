c48bc0c6a426c9e51f786c85036cdc9dc72de420
nathanfaucett <nathanfaucett@gmail.com>
2015-07-07 10:54:56 -0500

    updates

--- src/index.js
+++ src/index.js
@@ -1,14 +1,23 @@
-var isLength = require("is_length"),
+var isNative = require("is_native"),
+    isLength = require("is_length"),
     isObjectLike = require("is_object_like");
 
 
-var objectToString = Object.prototype.toString;
+var objectToString = Object.prototype.toString,
+    nativeIsArray = Array.isArray,
+    isArray;
 
 
-module.exports = Array.isArray || function isArray(obj) {
-    return (
-        isObjectLike(obj) &&
-        isLength(obj.length) &&
-        objectToString.call(obj) === "[object Array]"
-    ) || false;
-};
+if (isNative(nativeIsArray)) {
+    isArray = nativeIsArray;
+} else {
+    isArray = function isArray(value) {
+        return (
+            isObjectLike(value) &&
+            isLength(value.length) &&
+            objectToString.call(value) === "[object Array]"
+        ) || false;
+    };
+}
+
+module.exports = isArray;
